7124e3ad2dfb1c9d07275b2510ea1ceceb58077e
Lyle Rallos <chill.99.lr@gmail.com>
2020-08-24 20:38:07 +0800

    race Track
    downloaded a path creator

--- KartKart Racing Game/Assets/Scripts/playerMovement.cs
+++ KartKart Racing Game/Assets/Scripts/playerMovement.cs
@@ -7,45 +7,50 @@ public class playerMovement : MonoBehaviour
     // reference for rigid body
     public Rigidbody theRB; 
 
-    public float forwardAccel = 8f, reverseAccel = 4f, maxSpeed = 50f, turnStrength = 180, gravityForce = 10f, dragOnGround = 8f;
+    //variables needed for the movement of the car
+    public float forwardAccel = 8f, reverseAccel = 4f, maxSpeed = 50f, turnStrength = 180, gravityForce = 10f, dragOnGround = 3f;
 
     private float speedInput, turnInput;
 
+    // To know if car is on the ground
     private bool grounded;
 
     public LayerMask whatisGround;
     public float groundRayLength = .5f;
     public Transform groundRayPoint;
 
+    // to access the tranform rotation of the wheels on the car
     public Transform leftFrontWheel, rightFrontWheel;
     public float maxWheelTurn = 20f;
     void Start()
     {
+        // makes the sphere not part of the parent so as to make the car follow the sphere
         theRB.transform.parent = null;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // gives the car forward and backward movement
         speedInput = 0f;
         if (Input.GetAxis("Vertical")> 0)
         {
-            speedInput = Input.GetAxis("Vertical") * forwardAccel * 900f;
+            speedInput = Input.GetAxis("Vertical") * forwardAccel * 500f;
 
         }else if (Input.GetAxis("Vertical") < 0)
         {
-            speedInput = Input.GetAxis("Vertical") * reverseAccel * 600f;
+            speedInput = Input.GetAxis("Vertical") * reverseAccel * 300f;
         }
-
+        // lets the car rotate at the x plane
         turnInput = Input.GetAxis("Horizontal");
-
+        transform.position = theRB.transform.position;
         if (grounded)
         {
+            // let's the player rotate only when moving forward or backward only on ground
             transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, turnInput * turnStrength * Time.deltaTime * Input.GetAxis("Vertical"), 0f));
 
-            transform.position = theRB.transform.position;
         }
-
+        // rotates the wheels to make it look natural
         leftFrontWheel.localRotation = Quaternion.Euler(leftFrontWheel.localRotation.eulerAngles.x, turnInput * maxWheelTurn, leftFrontWheel.localRotation.eulerAngles.z);
         rightFrontWheel.localRotation = Quaternion.Euler(rightFrontWheel.localRotation.eulerAngles.x, turnInput * maxWheelTurn, rightFrontWheel.localRotation.eulerAngles.z);
     
@@ -53,16 +58,17 @@ public class playerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        // determines whether or not the car is in the air or in the ground
         grounded = false;
         RaycastHit hit;
-
-        if(Physics.Raycast(groundRayPoint.position, -transform.up, out hit, groundRayLength, whatisGround));
+        
+        if (Physics.Raycast(groundRayPoint.position, -transform.up, out hit, groundRayLength, whatisGround))
         {
             grounded = true;
-
+            
             transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
         }
-
+        
         if (grounded)
         {
             theRB.drag = dragOnGround;
@@ -72,8 +78,9 @@ public class playerMovement : MonoBehaviour
             }
         }else
         {
+            // adds acceleration when going from the ground to the air
             theRB.drag = 0.1f;
-            theRB.AddForce(Vector3.up * -gravityForce * 100);
+            theRB.AddForce(Vector3.up * -gravityForce * 100f);
         }
     }
 }
