8245a233c44e3380d3c000fb1e8dcdbf2473c396
Lyle Rallos <chill.99.lr@gmail.com>
2020-08-24 17:02:49 +0800

    CarController
    Added free models from Kenney Assets
    Made player/car controller to move

---
+++ KartKart Racing Game/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    // reference for rigid body
+    public Rigidbody theRB; 
+
+    public float forwardAccel = 8f, reverseAccel = 4f, maxSpeed = 50f, turnStrength = 180, gravityForce = 10f, dragOnGround = 8f;
+
+    private float speedInput, turnInput;
+
+    private bool grounded;
+
+    public LayerMask whatisGround;
+    public float groundRayLength = .5f;
+    public Transform groundRayPoint;
+
+    public Transform leftFrontWheel, rightFrontWheel;
+    public float maxWheelTurn = 20f;
+    void Start()
+    {
+        theRB.transform.parent = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speedInput = 0f;
+        if (Input.GetAxis("Vertical")> 0)
+        {
+            speedInput = Input.GetAxis("Vertical") * forwardAccel * 900f;
+
+        }else if (Input.GetAxis("Vertical") < 0)
+        {
+            speedInput = Input.GetAxis("Vertical") * reverseAccel * 600f;
+        }
+
+        turnInput = Input.GetAxis("Horizontal");
+
+        if (grounded)
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, turnInput * turnStrength * Time.deltaTime * Input.GetAxis("Vertical"), 0f));
+
+            transform.position = theRB.transform.position;
+        }
+
+        leftFrontWheel.localRotation = Quaternion.Euler(leftFrontWheel.localRotation.eulerAngles.x, turnInput * maxWheelTurn, leftFrontWheel.localRotation.eulerAngles.z);
+        rightFrontWheel.localRotation = Quaternion.Euler(rightFrontWheel.localRotation.eulerAngles.x, turnInput * maxWheelTurn, rightFrontWheel.localRotation.eulerAngles.z);
+    
+    }
+
+    private void FixedUpdate()
+    {
+        grounded = false;
+        RaycastHit hit;
+
+        if(Physics.Raycast(groundRayPoint.position, -transform.up, out hit, groundRayLength, whatisGround));
+        {
+            grounded = true;
+
+            transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+        }
+
+        if (grounded)
+        {
+            theRB.drag = dragOnGround;
+            if(Mathf.Abs(speedInput)> 0)
+            {
+                theRB.AddForce(transform.forward * speedInput);
+            }
+        }else
+        {
+            theRB.drag = 0.1f;
+            theRB.AddForce(Vector3.up * -gravityForce * 100);
+        }
+    }
+}
